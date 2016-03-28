eafa18b77f35be85247a1ec7bef8abe6d29357c7
BrookeH <Brooke@Brookes-MacBook-Pro-5.local>
2016-03-28 18:07:36 +0100

    got ball speed working

--- gamev2.py
+++ gamev2.py
@@ -1,4 +1,4 @@
-import serialBox, time
+import serialBox, time, math
 
 window_width = 80
 window_height = 20
@@ -16,7 +16,7 @@ class Game:
         ball_y = int(window_height/2)
         ball_radius = 1
         ball_color = serialBox.colors.GREEN
-        self.ball = Ball(ball_x, ball_y, ball_radius, ball_color, 1, self.screen)
+        self.ball = Ball(ball_x, ball_y, ball_radius, ball_color, 0.00009, self.screen)
       
         self.paddles = {}
         paddle_height = 3
@@ -34,13 +34,17 @@ class Game:
 
     def update(self):
         self.ball.move()
+        if self.ball.hit_left():
+            self.scorePlayer2 += 1
+        elif self.ball.hit_right():
+            self.scorePlayer1 += 1 
         if self.paddle_ball_collision():
             self.ball.bounce('x')
 
     def paddle_ball_collision(self):
-        if self.ball.dir_x == 1 and self.ball.y >= self.paddles['player2'].y and self.ball.y <= (self.paddles['player2'].y + self.paddles['player2'].length) and self.ball.x == (self.paddles['player2'].x - 1):
+        if self.ball.dir_x == 1 and self.ball.y >= self.paddles['player2'].y and self.ball.y <= (self.paddles['player2'].y + self.paddles['player2'].length) and self.ball.x >= (self.paddles['player2'].x):
             return True
-        elif self.ball.dir_x == -1 and self.ball.y >= self.paddles['player1'].y and self.ball.y <= (self.paddles['player1'].y + self.paddles['player1'].length) and self.ball.x == (self.paddles['player1'].x - 1):
+        elif self.ball.dir_x == -1 and self.ball.y >= self.paddles['player1'].y and self.ball.y <= (self.paddles['player1'].y + self.paddles['player1'].length) and self.ball.x <= (self.paddles['player1'].x + 1):
             return True
         else:
             return False
@@ -56,9 +60,12 @@ class Paddle(serialBox.line):
 
     def move(self,y):
         if y < window_height - self.length and y > 0:
-            self.clear(self.screen)
-            self.y = y
-            self.draw(self.screen)
+            if int(self.y != int(y)):
+                self.clear(self.screen)
+                self.y = y
+                self.draw(self.screen)
+            else:
+                self.y = y
 
 class Ball(serialBox.rect):
     """docstring for Ball"""
@@ -71,10 +78,16 @@ class Ball(serialBox.rect):
         self.draw(self.screen)
 
     def move(self):
-        self.clear(self.screen)
-        self.x += (self.dir_x * self.speed)
-        self.y += (self.dir_y * self.speed)
-        self.draw(self.screen)
+        new_x = self.x + (self.dir_x * self.speed)
+        new_y = self.y + (self.dir_y * self.speed)
+        if int(new_x) != int(self.x) or int(new_y) != int(self.y):
+            self.clear(self.screen)
+            self.x = new_x
+            self.y = new_y
+            self.draw(self.screen)
+        else:
+            self.x = new_x
+            self.y = new_y
 
         if self.hit_ceiling() or self.hit_floor():
             self.bounce('y')
@@ -88,25 +101,25 @@ class Ball(serialBox.rect):
             self.dir_y *= -1
 
     def hit_ceiling(self):
-        if self.dir_y == -1 and self.y == 1: 
+        if self.dir_y == -1 and int(self.y) == 0: 
             return True
         else:
             return False
 
     def hit_floor(self):
-        if self.dir_y == 1 and self.y == self.screen.height - 1:
+        if self.dir_y == 1 and self.y >= self.screen.height - 1:
             return True
         else:
             return False
 
     def hit_left(self):
-        if self.x == 1:
+        if self.x <= 1:
             return True
         else:
             return False
 
     def hit_right(self):
-        if self.x == self.screen.width -1:
+        if self.x >= self.screen.width -1:
             return True
         else:
             return False
@@ -114,15 +127,14 @@ class Ball(serialBox.rect):
 
 
 game = Game()
-time.sleep(1)
+
 import Tkinter as tk
 root = tk.Tk()
 while True:
     game.update()
     y = root.winfo_pointery()
-    game.paddles['player1'].move(y/5)
-    time.sleep(1)
-
+    game.paddles['player1'].move(y/10)
+    time.sleep(1/24)
 
 
 
